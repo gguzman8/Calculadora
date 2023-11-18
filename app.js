b115fdc7e6a05845af0ed4b9f7c3750fdc91fd66
gguzman8 <gguzman8@ucol.mx>
2023-11-17 20:30:15 -0600

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-function limpiar() {
-    document.getElementById('formulario').reset();
-    
-}
+let boton = document.querySelector("#boton");
+boton.addEventListener("click",function(){
+    console.log("Dimos Click en el boton");
+    boton.classList.toggle('verde');
+});
+
+boton.addEventListener("mouseover",function(){
+    document.querySelector("#over").innerHTML = ("Estamos sobre el boton");
+});
+
+boton.addEventListener("mouseout",function(){
+    document.querySelector("#over").innerHTML = ("Salí del boton");
+});
+
+window.addEventListener("keydown" , (e) => {
+    document.querySelector("#a").innerHTML = ("Pulso una tecla");
+    document.querySelector("#teclas").innerHTML = (String.fromCharCode (e.keyCode));
+} );
+
+window.addEventListener("keyup" , (e) => {
+    document.querySelector("#a").innerHTML = ("Deje de presionar una tecla");
+} );
+
+window.addEventListener("load", (e) => {
+    document.querySelector("#cargue").innerHTML = ("Termine de cargar la página");
+});
 
 function suma() {
     let x = parseInt(document.getElementById('numero1').value);
